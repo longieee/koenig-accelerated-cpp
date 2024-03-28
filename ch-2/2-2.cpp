d28cc77dbc4849a11d4b1a6131dc18f2c2b1d3ed
longieee <long.dnguy@gmail.com>
2024-03-29 01:22:51 +0700

    add 2-2

---
+++ ch-2/2-2.cpp
@@ -0,0 +1,72 @@
+// Exercise: Change the framing program so that it uses a different amount of
+// space to separate the sides from the greeting than it uses to separate the
+// top and bottom borders from the greeting.
+#include <iostream>
+#include <string>
+
+// say what standard-library names we use
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+int main()
+{
+    // ask for the person's name
+    cout << "Please enter your first name: ";
+
+    // read the name
+    string name;
+    cin >> name;
+
+    // build the message that we intend to write
+    const string greeting = "Hello, " + name + "!";
+
+    // the number of blanks surrounding the greeting
+    // Original: same padding is used for all sides of the greeting.
+    // To have different padding for the sides and top/bottom, using 2 variables
+    const int pad_vertical = 1;
+    const int pad_horizontal = 5;
+
+    // the number of rows and columns to write
+    const int rows = pad_vertical * 2 + 3;
+    const string::size_type cols = greeting.size() + pad_horizontal * 2 + 2;
+
+    // write a blank line to separate the output from the input
+    cout << endl;
+
+    // write `rows` rows of output
+    // invariant: we have written `r` rows so far
+    for (int r = 0; r != rows; ++r)
+    {
+        string::size_type c = 0;
+        // invariant: we have written c characters so far in the current row
+        while (c != cols)
+        {
+            // is it time to write the greeting?
+            if (r == pad_vertical + 1 && c == pad_horizontal + 1)
+            {
+                cout << greeting;
+                c += greeting.size();
+            }
+            else
+            {
+                // are we on the border?
+                if (r == 0 || r == rows - 1 || c == 0 || c == cols - 1)
+                {
+                    cout << "*";
+                }
+                else
+                {
+                    cout << " ";
+                }
+
+                ++c;
+            }
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
