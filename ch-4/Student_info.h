d9b87a04bd7c407e7c13f0a10bed576267b48d18
longieee <long.dnguy@gmail.com>
2024-06-12 23:30:58 +0700

    add 4-1

---
+++ ch-4/Student_info.h
@@ -0,0 +1,20 @@
+#ifndef GUARD_Student_info
+#define GUARD_Student_info
+
+// Student_info.h header file
+#include <iostream>
+#include <string>
+#include <vector>
+
+struct Student_info
+{
+    std::string name;
+    double midterm, final;
+    std::vector<double> homework;
+}; // note the semicolon - it's required
+
+bool compare(const Student_info&, const Student_info&);
+std::istream& read(std::istream&, Student_info&);
+std::istream& read_hw(std::istream&, std::vector<double>&);
+
+#endif
