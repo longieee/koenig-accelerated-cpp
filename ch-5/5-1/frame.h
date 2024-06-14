d24c2958982326f4848d808698aef23c85a47c14
longieee <long.dnguy@gmail.com>
2024-06-14 22:14:37 +0700

    add 5.1

---
+++ ch-5/5-1/frame.h
@@ -0,0 +1,15 @@
+#ifndef FRAME_H
+#define FRAME_H
+
+#include <algorithm>
+#include <string>
+#include <vector>
+
+std::string::size_type width(const std::vector<std::string>&);
+std::vector<std::string> frame(const std::vector<std::string>&);
+std::vector<std::string> vcat(const std::vector<std::string>&,
+                              const std::vector<std::string>&);
+std::vector<std::string> hcat(const std::vector<std::string>&,
+                              const std::vector<std::string>&);
+
+#endif // FRAME_H
---
+++ ch-5/5-1/grade.h
@@ -0,0 +1,14 @@
+#ifndef GUARD_grade_h
+#define GUARD_grade_h
+
+// `grade.h'
+#include "Student_info.h"
+#include <vector>
+
+double grade(double, double, double);
+double grade(double, double, const std::vector<double>&);
+double grade(const Student_info&);
+bool fgrade(const Student_info&);
+std::vector<Student_info> extract_fails(std::vector<Student_info>&);
+
+#endif
