746f376003b43283d49043d8d78fd252cba14257
longieee <long.dnguy@gmail.com>
2024-06-21 21:39:32 +0700

    add primitive test

---
+++ ch-5/5-1/tests/test_permutation_index.cpp
@@ -0,0 +1,73 @@
+#include "../header/PermutationIndex.h"
+
+#include <iostream>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::istream;
+using std::string;
+using std::vector;
+
+void printline(const vector<string>& s, bool print_separator = false)
+{
+    vector<string>::const_iterator iter_line = s.begin();
+    while (iter_line != s.end())
+    {
+        cout << *iter_line;
+        if (print_separator)
+        {
+            cout << ", ";
+        }
+        else
+        {
+            cout << ' ';
+        }
+        iter_line++;
+    }
+    cout << endl;
+}
+
+int main()
+{
+
+    string s;
+    vector<string> page;
+    vector<Line> lines;
+    vector<WordPermutation> permutations;
+
+    cout << "Page content:" << endl;
+    page = read_page(cin, s);
+    lines = split_raw_page(page);
+
+    vector<Line>::iterator iter_lines = lines.begin();
+
+    // Print out the raw strings read as Line objects
+    while (iter_lines != lines.end())
+    {
+
+        cout << "Line number " << iter_lines->line_number << ": ";
+        printline(iter_lines->text, true);
+        iter_lines++;
+    }
+
+    // Print out permutation indexes
+    cout << "Permutations: " << endl;
+    iter_lines = lines.begin();
+    while (iter_lines != lines.end())
+    {
+        permutations = create_permutations(*iter_lines);
+        cout << "Line number " << iter_lines->line_number << ": " << endl;
+        vector<WordPermutation>::iterator iter_perm = permutations.begin();
+        while (iter_perm != permutations.end())
+        {
+            printline(iter_perm->text);
+            cout << iter_perm->index_word << endl;
+            iter_perm++;
+        }
+        iter_lines++;
+    }
+
+    return 0;
+}
